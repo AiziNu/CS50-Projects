27131c0f9c8cc9092a6748493257bb4a77683fc8
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-10 17:25:18 +0000

    Fri, May 10, 2024, 7:25 PM +02:00

---
+++ world/scrabble/scrabble.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+int calculate(string word){
+    int score = 0;
+    int letter_score[26]= {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+    for(int i=0; word[i] != '\0'; i++){
+        if(isalpha(word[i])){
+            score += letter_score[toupper(word[i])- "A"];
+        }
+    }
+    return score
+}
+
+
+int main(void) {
+    string word1 = get_string("Player 1: ");
+
+    string word2 = get_string("Player 2: ");
+
+    int score1 = calculate_score(word1);
+    int score2 = calculate_score(word2);
+
+    if (score1 > score2) {
+        printf("Player 1 wins!\n");
+    } else if (score1 < score2) {
+        printf("Player 2 wins!\n");
+    } else {
+        printf("Tie!\n");
+    }
+}

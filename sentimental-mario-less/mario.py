15e23d3b86e94e9d8f3d4d265d602e29d84682fb
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 10:55:29 +0000

    Sat, May 18, 2024, 12:55 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,8 +1,8 @@
 
-def get_int(n):
+def get_int(prompt):
     while True:
         try:
-            value = int(input(n))
+            value = int(input(prompt))
             if 1 <= value <= 8:
              return value
         except ValueError:
@@ -12,6 +12,6 @@ def draw_piramid():
     height = get_int("Enter the height of the pyramid: ")
 
     for i in range(1, height + 1):
-        print("" * (height-1) + "#" * i)
+        print(" " * (height-1) + "#" * i)
 
 draw_piramid()

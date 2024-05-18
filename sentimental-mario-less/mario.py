51e9c8096dd7226c065178f051c6632284e5978c
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 10:57:29 +0000

    Sat, May 18, 2024, 12:57 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -12,6 +12,7 @@ def draw_piramid():
     height = get_int("Enter the height of the pyramid: ")
 
     for i in range(1, height + 1):
-        print(" " * (height-1) + "#" * i)
+        print(" " * (height-1) + "#" * i, end="")
+        print()
 
 draw_piramid()

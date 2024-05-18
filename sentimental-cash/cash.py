1db7670b1e6f0a62b2f61a7caec3d6ad7bf43670
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 11:15:29 +0000

    Sat, May 18, 2024, 1:15 PM +02:00

---
+++ sentimental-cash/cash.py
@@ -0,0 +1,17 @@
+def get_int(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= 8:
+             return value
+        except ValueError:
+            continue
+
+def draw_piramid():
+    height = get_int("Enter the height of the pyramid: ")
+
+    for i in range(1, height + 1):
+        print(" " * (height -i) + "#" * i, end="")
+        print()
+
+draw_piramid()

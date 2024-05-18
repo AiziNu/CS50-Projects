fa9e23b15a860edb85598ab11cc65fa44821793b
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 11:38:14 +0000

    Sat, May 18, 2024, 1:38 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,4 +11,17 @@ def calculate_readability(text):
     L = (letters / words) *100
     S = (sentences / words) * 100
 
-    grade = round()
+    grade = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if grade < 1:
+    print("Before Grade 1")
+    elif grade >=16:
+     print("Grade 16+")
+   else:
+     print("Grade", grade)
+
+def main():
+    text = input("Enter some text: ")
+    calculate_readability(text)
+
+main()

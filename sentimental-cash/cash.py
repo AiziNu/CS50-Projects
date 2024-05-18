fee053e9a7539b5ee491d93c9c853fa51da95009
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 11:21:01 +0000

    Sat, May 18, 2024, 1:21 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,17 +2,28 @@ def get_float(prompt):
     while True:
         try:
             value = float(input(prompt))
-            if value >= 0
+            if value >= 0:
              return value
         except ValueError:
             print("Invalid input. Please enter a non-negative number.")
             continue
 
-def draw_piramid():
-    height = get_int("Enter the height of the pyramid: ")
+def calculate_coins():
+    change = get_float("Enter the amount of change owed: ")
 
-    for i in range(1, height + 1):
-        print(" " * (height -i) + "#" * i, end="")
-        print()
+    cents = round(change * 100)
+    quarters = 25
+    dimes = 10
+    nickles = 5
+    pennies = 1
 
-draw_piramid()
+    num_coins = 0
+    num_coins += cents // quarters
+    cents %= quarters
+    num_coins += cents // dimes
+    cents %= dimes
+    num_coins += cents // nickels
+    cents %= nickels
+    num_coins += cents // pennies
+
+    print(num_coins)

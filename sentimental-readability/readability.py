160b368544a35fc72bd041e0ff28e78e80dde893
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-18 11:50:56 +0000

    Sat, May 18, 2024, 1:50 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,24 +5,30 @@ def get_string(prompt):
 
 def calculate_readability():
     text = get_string("Enter some text: ")
-    
+
+    # Count the number of letters
     letters = len(re.findall(r'[a-zA-Z]', text))
 
+    # Count the number of words
     words = len(re.findall(r'\b\w+\b', text))
 
-    sentences = text.count('.')+ text.count('!') + text.count('?')
+    # Count the number of sentences
+    sentences = text.count('.') + text.count('!') + text.count('?')
 
-    L = (letters / words) *100
+    # Calculate the average number of letters and sentences per 100 words
+    L = (letters / words) * 100
     S = (sentences / words) * 100
 
-    grade = round(0.0588 * L - 0.296 * S - 15.8)
+    # Calculate the grade level using the Coleman-Liau formula
+    index = round(0.0588 * L - 0.296 * S - 15.8)
 
-    if grade < 1:
-      print("Before Grade 1")
-    elif grade >=16:
-     print("Grade 16+")
+    # Print the grade level
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
     else:
-     print(f"Grade {grade} ")
+        print(f"Grade {index}")
 
 calculate_readability()
 

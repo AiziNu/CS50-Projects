41dfdd9c8e68186a0335d72c0a97a842f7221210
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-20 20:38:52 +0000

    Mon, May 20, 2024, 10:38 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 sqlite3 fiftyville.db
-.schema crime_scene_reports
+sqlite> .tables
+sqlite> .schema crime_scene_reports
+
+sqlite> SELECT *
+FROM crime_scene_reports
+WHERE date = 'YYYY-MM-DD' AND location = 'Crime Location';

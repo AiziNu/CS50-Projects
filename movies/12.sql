4239dca9e4458f4020b4a91666414269dff497f8
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-20 17:55:56 +0000

    Mon, May 20, 2024, 7:55 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,13 +1,13 @@
 SELECT title
 FROM movies
-WHERE id IN(
+WHERE id IN (
     SELECT movie_id
     FROM stars
-    WHERE person_id IN(
+    WHERE person_id IN (
         SELECT id
         FROM people
         WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence'
     )
 )
 GROUP BY title
-HAVING COUNT(DISTINCT person_id) = 2;
+HAVING COUNT(DISTINCT actor_id) = 2;

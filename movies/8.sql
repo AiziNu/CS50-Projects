75a4253ea8deee46155e85369068f845644fba44
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-20 16:59:14 +0000

    Mon, May 20, 2024, 6:59 PM +02:00

---
+++ movies/8.sql
@@ -0,0 +1,11 @@
+SELECT name
+FROM people
+WHERE id IN(
+    SELECT person_id
+    FROM roles
+    WHERE movie_id(
+        SELECT id
+        FROM movies
+        WHERE title = 'Toy Story'
+    )
+);

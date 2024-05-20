1765c8add9108af21f5b8eb2326794e55534df87
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-20 17:09:14 +0000

    Mon, May 20, 2024, 7:09 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,13 +1,5 @@
 SELECT name
 FROM people
 WHERE id IN(
-    SELECT person_id
-    FROM stars
-    WHERE movie_id = (
-        SELECT id
-        FROM movies
-        WHERE year = 2004
-    )
+    SELECT 
 )
-AND birth IS NOT NULL
-ORDER BY birth;

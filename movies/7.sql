48731da2eba748b80fbbed852ef8ab414970b3ea
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-20 16:55:56 +0000

    Mon, May 20, 2024, 6:55 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,5 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 AND movies.rating IS NOT NULL
-ORDER BY ratings.rating DESC, title;
+SELECT title, rating
+FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.year = 2010 AND ratings.rating IS NOT NULL
+ORDER BY ratings.rating DESC, movies.title;

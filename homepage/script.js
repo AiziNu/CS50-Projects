7acfdd3618111bbfeb14cf92cd15dbd3767965bb
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-21 19:18:30 +0000

    Tue, May 21, 2024, 9:18 PM +02:00

--- homepage/script.js
+++ homepage/script.js
@@ -4,7 +4,7 @@ let button = document.getElementById('main_btn');
 // Add event listener to the button
 button.addEventListener('click', function() {
     // Redirect to another website
-    window.location.href = 'https://www.example.com';
+    window.location.href = 'https://codecoredevs.com/';
 
     // Or change the background image of the webpage
     document.body.style.backgroundImage = "url('path-to-your-image.jpg')";

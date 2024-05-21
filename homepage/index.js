a0dfcf1b4352a722f66f12adc7d0e18dbf62a86d
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-21 19:22:31 +0000

    Tue, May 21, 2024, 9:22 PM +02:00

--- homepage/index.js
+++ homepage/index.js
@@ -5,7 +5,4 @@ let button = document.getElementById('main_btn');
 button.addEventListener('click', function() {
     // Redirect to another website
     window.location.href = 'https://codecoredevs.com/';
-
-    // Or change the background image of the webpage
-    document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5uxxPvYY8Rv59YsdLzGBveIy37gPrieboPw&s')";
 });

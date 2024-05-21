847bfa5e05aa790f0bf2459c298be6ab48bc0e3f
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-21 17:31:37 +0000

    Tue, May 21, 2024, 7:31 PM +02:00

--- trivia/index.js
+++ trivia/index.js
@@ -1,3 +1,7 @@
 const buttons = document.querySelectorAll(.answer).forEach(button=>{
-    
+    button.addEventListener('click', function(){
+        let isCorrect = this.dataset.correct === 'true';
+        this.className = isCorrect ? 'correct' : 'incorrect';
+        document.getElementById('feedback1').textContent = isCorrect ? 'Correct!' : 'Incorrect';
+    })
 })

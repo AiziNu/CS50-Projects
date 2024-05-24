eb34eb8d36d2caea54a579b12308489f126d4a47
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-24 07:22:16 +0000

    Fri, May 24, 2024, 9:22 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -86,8 +86,7 @@ def buy():
         db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", cost, user_id)
 
         return redirect("/")
-   
-    else:
+   else:
         return render_template("buy.html")
 
 

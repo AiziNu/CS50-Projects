98b307e1ad3f0410b449de5c64621c13c222270a
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 10:56:53 +0000

    Mon, May 27, 2024, 12:56 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,11 +3,9 @@ sqlite3 fiftyville.db
 sqlite> .tables
 sqlite> .schema crime_scene_reports
 
-sqlite> SELECT *
+SELECT *
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
-.schema interviews
-SELECT *
-FROM interviews;.tables
 
-SELECT * FROM interviews WHERE LOWER(transcript) LIKE '%bakery%';
+SELECT * FROM interviews WHERE transcript LIKE '%bakery%';
+

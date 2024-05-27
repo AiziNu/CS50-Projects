5f7ef2ff9476898d45b38a9c82f97b3716d62405
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 11:50:33 +0000

    Mon, May 27, 2024, 1:50 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,4 +12,4 @@ SELECT * FROM interviews WHERE transcript LIKE '%bakery%';
 SELECT * FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute BETWEEN 10 AND 15;
 --license_plate 13FNH73
 --Witness2 Eugene, ATM transactions
-
+SELECT * FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute BETWEEN 10 AND 15;

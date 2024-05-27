e1a9f59036b3df42b69cd2e06d300ad95ec5b33a
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 11:58:24 +0000

    Mon, May 27, 2024, 1:58 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,3 +13,5 @@ SELECT * FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 
 --license_plate 13FNH73
 --Witness2 Eugene, ATM transactions
 SELECT * FROM atm_transactions WHERE atm_location = "Leggett Street" AND year = 2023 AND month = 7 AND day = 28;
+--add name of withdraws
+SELECT a.*, p.name FROM atm_transactions a JOIN bank_accounts b ON a.account_number = b.account_number JOIN people p ON b.person_id = p.id WHERE a.atm_location = "Leggett Street" AND a.year = 2023 AND a.month = 7 AND a.day = 28 AND a.transaction_type = "withdraw";

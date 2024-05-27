d03b0ab7c1048154acb4675cf390b26546a8f281
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 10:58:24 +0000

    Mon, May 27, 2024, 12:58 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,4 @@ WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 
 SELECT * FROM interviews WHERE transcript LIKE '%bakery%';
 
+SELECT * FROM atm_transactions WHERE date = '2023-07-28' AND location = 'Leggett Street';

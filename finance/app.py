d43e0715b2663422582f7e95d08efcea90bf889e
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 19:22:32 +0000

    Mon, May 27, 2024, 9:22 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -72,16 +72,17 @@ def buy():
         price = quote["price"]
         total_cost = int(shares)*price
         cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id=session[user_id])[0]["cash"]
-        cost = shares * stock["price"]
 
         if cash < total_cost:
             return apology("can't afford")
 
         db.execute("UPDATE users SET cash = cash - :total_cost WHERE id = :user_id",
                    total_cost=total_cost, user_id = session["user_id"])
-        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id, :symbol,)", user_id, stock["symbol"], shares, stock["price"])
-
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id, :symbol, :shares. :price)",
+                    user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+        flash(f"Bought {shares} shares of {symbol} for {usd(total_cost)}.")
         return redirect("/")
+    
     else:
         return render_template("buy.html")
 

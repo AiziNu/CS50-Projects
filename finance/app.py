371108a6b95ff67198422c72f1cc0d070e1c38f9
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 20:48:31 +0000

    Mon, May 27, 2024, 10:48 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -221,6 +221,12 @@ def sell():
                     price = quote["price"]
                     total_sale = shares * price
 
+                    #Update users
+                    db.execute("UPDATE users SET cash = cash + :total_sale WHERE id = :user_id",
+                               total_sale=total_sale ,user_id= session["user_id"])
+                    #add users table
+                    db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (?, ?, ?, ?)", user_id, symbol, -shares, price)
+
 
 
         price = stock["price"]

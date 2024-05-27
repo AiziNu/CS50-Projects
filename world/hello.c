912971f0dcd6e8405a17a17328954fbe86e5a37e
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 16:15:02 +0000

    Mon, May 27, 2024, 6:15 PM +02:00

--- world/hello.c
+++ world/hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void)
+{
+    // Prompt the user for their name
+    string name = get_string("What is your name?\n");
+
+    // Print a greeting to the user
+    printf("hello, %s\n", name);
+
+    return 0;
 }

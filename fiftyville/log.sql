a816716b15ea6305b598b03e6b31cbc8e1cb6701
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-27 13:13:07 +0000

    Mon, May 27, 2024, 3:13 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -28,4 +28,5 @@ JOIN airports origin ON f.origin_airport_id = origin_id
 JOIN airports destination ON f.destination_airport_id = destination_id
 WHERE origin.id = 8 AND f.year = 2023 AND f.month = 7 AND f.day = 29 ORDER BY f.hour, f.minute;
 
---we have in
+--we have ainfo we need to combine all info
+

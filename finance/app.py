1bb9855694318f9b82c0809831efb84f582c83fe
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-05-28 07:26:27 +0000

    Tue, May 28, 2024, 9:26 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -146,7 +146,6 @@ def quote():
         stock = lookup(symbol)
         if not stock:
             return apology("invalid symbol",400)
-
         return render_template("quoted.html", quote=quote)
     else:
         return render_template("quote.html")

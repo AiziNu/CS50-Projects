cb4987e9b6627f967b6ff14d48014088ba64801f
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-01 17:10:36 +0000

    Sat, Jun 1, 2024, 7:10 PM +02:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,4 +1,15 @@
 # create formula func
 
 def einstein(mass_input):
-    
+    c = 300000000
+    mass = int(mass_input)
+    formula = mass * c**2
+    return formula
+
+def main():
+    user_input= input("Enter mass: ")
+    result = einstein(user_input)
+    print(result)
+
+
+main()

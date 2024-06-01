712aba1c6f623f51fd37774c6f392ed489a3b25f
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-01 16:53:06 +0000

    Sat, Jun 1, 2024, 6:53 PM +02:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,5 +1,17 @@
 # Create function convert accept str as a input and converts
 
 # creare var is is equal to input to get a str
-def convert():
-    s_input = input("Get emojis: ").replace()
+def convert(input_str):
+      # Replace :) with 🙂
+    output_str = input_str.replace(':)', '🙂')
+    # Replace :( with 🙁
+    output_str = output_str.replace(':(', '🙁')
+    return output_str
+
+
+def main():
+
+    user_input = input("Enter sentense with Emojis: ")
+    result = convert(user_input)
+    print(result)
+

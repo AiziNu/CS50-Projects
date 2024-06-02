d14fecb5a9877293f0dd6a3f4c69f33e2433c21b
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 14:49:42 +0000

    Sun, Jun 2, 2024, 4:49 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,19 @@
 def main():
     user_input = input("What time is it? ").strip().split(":")
     print(user_input)
+    if convert(user_input):
+        
 
 def convert(time):
-    
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
+    else:
+        return
+
 
 
 if __name__ == "__main__":

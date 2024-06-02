1e2ede6beba4a89d38534104e05782990a9d9de9
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 14:22:18 +0000

    Sun, Jun 2, 2024, 4:22 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,13 +1,31 @@
 
 def main():
       # Prompt the user for an arithmetic expression
-    expression = input('Expression (x, y,z): ').strip()
+    expression = input('Expression (x, y, z): ').strip()
 
     # Split the expression into components
     x, y, z = expression.split(" ")
 
     # Convert x and z to integers
     x = int(x)
-    y = int(y)
-    
+    z = int(y)
+
+    # Perform the appropriate arithmetic operation
+    if y == "+":
+        result =  x + z
+    elif y == "-":
+        result =  x - z
+    elif y == "*":
+        result =  x * z
+    elif y == "/":
+        result =  x / z
+    else:
+        print("Invalid operator")
+        return
+
+    print(f"{result:1f}")
+
+
+main()
+
 

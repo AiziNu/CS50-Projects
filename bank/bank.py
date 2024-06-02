23276465dcdd49b9799a006285d6f1975fbca800
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 10:59:48 +0000

    Sun, Jun 2, 2024, 12:59 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,5 +1,7 @@
 # create var greeting with user prompt
-greeting = input("Please enter your greet. ").strip()
+greeting = input("Please enter your greet. ").strip().split(" ")
+print(greeting)
+
 
 #create conditions
 if greeting.lower() == "hello":

b3e7b589d6c38a645bb4986bf8e4fc7c2e87a265
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 11:34:54 +0000

    Sun, Jun 2, 2024, 1:34 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,4 +6,13 @@ if file_name.endswith(".gif"):
     print("image/gif")
 elif file_name.endswith(".jpg") or file_name.endswith(".jpeg"):
     print("image/jpeg")
-elif
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("image/png")
+elif file_name.endswith(".png"):
+    print("image/png")
+
+

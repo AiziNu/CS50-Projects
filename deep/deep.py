f460bb817ec859882ee2b90b22a571cfd41ac776
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 10:50:33 +0000

    Sun, Jun 2, 2024, 12:50 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 ## get input from user
-user_thought = input("Enter the aswer: ")
+user_thought = input("Enter the aswer: ").strip()
 
 # create conditions
 if user_thought == '42' or user_thought.lower() == 'forty two' or user_thought.lower() == 'forty-two':

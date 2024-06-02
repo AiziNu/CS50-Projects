ce8721bac90cfeb75d1715a875c0b65239fcccd6
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 10:56:38 +0000

    Sun, Jun 2, 2024, 12:56 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,8 @@
-greeting
+# create var greeting with user prompt
+greeting = input("Please enter your greet. ").strip()
+
+#create conditions
+if greeting.lower() == "hello":
+    print("$0")
+elif greeting[0].lower() == "h":
+    print("$20")

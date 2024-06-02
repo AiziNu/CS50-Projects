d1906fc2f761274de21dab699e553939f634b680
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-02 11:30:59 +0000

    Sun, Jun 2, 2024, 1:30 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,4 +3,7 @@ file_name = input('Enter file. ').strip().lower()
 
 # create conditions and return back
 if file_name.endswith(".gif"):
-    print()
+    print("image/gif")
+elif file_name.endswith(".jpg") or file_name.endswith(".jpeg"):
+    print("image/jpeg")
+elif

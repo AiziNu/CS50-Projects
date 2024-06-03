6536dd6add9a472eabb15c79aa869061cf146684
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-03 17:13:23 +0000

    Mon, Jun 3, 2024, 7:13 PM +02:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,6 +3,7 @@
 def main():
     user_input = input("Enter camelCase: ")
     snake_case = convert_snakeCase(user_input)
+    return snake_case
 
 
 # create funct with converts to snake_case
@@ -10,6 +11,11 @@ def convert_snakeCase(s):
     new_string = ""
     for i in s:
         if (i.isupper()):
-            new_string += "*"+1
+            new_string +="*"+1
         else:
             new_string +=i
+    x = new_string.split("*")
+    x.remove('')
+    print(x)
+
+main()

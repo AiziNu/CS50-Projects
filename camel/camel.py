b013a82c8854a1a657cf1513805f6c1c3f82a91f
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-03 11:34:42 +0000

    Mon, Jun 3, 2024, 1:34 PM +02:00

---
+++ camel/camel.py
@@ -0,0 +1,10 @@
+#create main func for get user prompt
+
+def main():
+    user_input = input("Enter camelCase: ")
+    snake_case = convert_snakeCase(user_input)
+
+
+# create funct with converts to snake_case
+def convert_snakeCase(s):
+    string = s.

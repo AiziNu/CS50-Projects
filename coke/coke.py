5539ac15389bd9dfec6867fceb95fc3765cdf942
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-05 10:44:07 +0000

    Wed, Jun 5, 2024, 12:44 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,14 +3,14 @@ def main():
     accepted_coins = [5,10,25]  #accepted denomanation
 
     while amount_due > 0:
-
+        print(f"Amount Due: {amount_due}")
         coin = int(input("Insert Coin: "))
 
         if coin in accepted_coins:
             amount_due -= coin
             print(f"Amount Due: {amount_due}")
         else:
-            print(f"Amount Due: {amount_due} cents")
+            print("Invalid coin insert 5, 10 or 25 cents")
 
     change_owed = -amount_due  # calculate change owed
     print(f"Change Owed: {change_owed} cents")

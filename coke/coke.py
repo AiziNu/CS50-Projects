30463634e8b58c8e9ea2e7cb7bdbe0783221da45
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-05 07:36:23 +0000

    Wed, Jun 5, 2024, 9:36 AM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,6 +1,6 @@
 def main():
     amount_due = 50  # tottal amount of one bottle of coce
-    accepted_coins = [10,25,50]  #accepted denomanation
+    accepted_coins = [5,10,25]  #accepted denomanation
 
     while amount_due > 0:
         print(f"Amount Due: {amount_due} cents")

7b8d0386629ca7ae7d53a1d865b024b98f3ceb94
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-05 11:28:53 +0000

    Wed, Jun 5, 2024, 1:28 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,12 +7,15 @@ def main():
 
 
 def is_valid(s):
-    #we will loop through s
-    for char in s:
-        if 2 <=len(s)<=6 and s[char].endswith(int) and s[char].startswith(str):
-            return True
-        else: False
-        
+    # we check all condition one by one
+    #1.length of s it has ti be between 2 to 6 inclusiev
+    if len(s)< 2 or len(s) > 6:
+        return False
+
+    #2. chech if plates starts with 2 letters
+    of 
+
+
 
 
 main()

ac51d05c551a3d808acb436c7e101ec374e87015
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-05 18:59:34 +0000

    Wed, Jun 5, 2024, 8:59 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,3 +1,13 @@
 def main():
+    import sys
     buy_list = {}
-    
+
+    while True:
+        try:
+             item = input().strip().lower()
+             if item in buy_list:
+                 buy_list[item] +=1
+             else:
+                 buy_list[item] +=1
+
+

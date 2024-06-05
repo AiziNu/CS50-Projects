887fef542a581b077ab09a5799c4e876ecdd7f03
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-05 10:59:49 +0000

    Wed, Jun 5, 2024, 12:59 PM +02:00

---
+++ twttr/twttr.py
@@ -0,0 +1,15 @@
+def main():
+    user_input = input("Enter your tweet: ")
+    result = convert_twttr(user_input)
+    print(result)
+
+
+
+def convert_twttr(str_input):
+    new_string = ""
+
+    for char in str_input:
+        print(c, end="")
+
+
+main()

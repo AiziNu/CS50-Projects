e6a0ef077305b2b970a00e8b4000345071e3b0a3
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 18:26:27 +0000

    Mon, Jun 10, 2024, 8:26 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,6 +2,6 @@ import emoji
 
 
 user_input = input("Input: ")
-result = emoji.emojize("Output: ", user_input, language='alias')
+result = emoji.emojize(f"Output: {user_input} " , language='alias')
 print(result)
 

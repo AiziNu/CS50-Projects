abff29cb019b1ce9576972a77404b23fb5817072
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 20:14:08 +0000

    Mon, Jun 10, 2024, 10:14 PM +02:00

--- game/game.py
+++ game/game.py
@@ -5,6 +5,13 @@ def main():
     # get user promt n until what number they want to guess
     print("Enter number of Level from 1 to 100 ")
     n = int(input("Level: "))
-    guess = random.randrange(1, n+1)
+    rand_guess = random.randrange(1, n+1)
 
-    if guess 
+    guess = int(input("Guess: "))
+
+    while True:
+        if guess < rand_guess:
+            print("Too small")
+        elif guess > rand_guess
+
+

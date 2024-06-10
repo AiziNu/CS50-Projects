7c3c1b8935c4ff333800cd69149210b9669059eb
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 20:25:08 +0000

    Mon, Jun 10, 2024, 10:25 PM +02:00

--- game/game.py
+++ game/game.py
@@ -4,19 +4,33 @@ import random
 def main():
     # get user promt n until what number they want to guess
     print("Enter number of Level from 1 to 100 ")
-    n = int(input("Level: "))
-    rand_guess = random.randrange(1, n+1)
 
-    guess = int(input("Guess: "))
-
-    while True:
-        if guess < rand_guess:
-            print("Too small")
-        elif guess > rand_guess
-
-def get_guess():
     while True:
         try:
-            guess = int(input("Guess: "))
-            if
+            n = int(input("Level: "))
+            if 1 <=n <=100:
+                break
+            else:
+                print("Please enter number between 1 to 100")
+        except ValueError:
+            print("Enter valid integer")
+    #generate random integer
+    rand_guess = random.randint(1, n+1)
+
+
+    #loop continiusly until user get correct number
+    while True:
+         guess = int(input("Guess: "))
+            #check guess and print too low or too high
+         if guess < rand_guess:
+              print("Too small")
+         elif guess > rand_guess:
+              print("Too large")
+         else:
+             print("Just right")
+             break
+main()
+
+
+
 

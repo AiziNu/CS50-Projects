13c97924900a2eb037ff9897b84276b3dda50eab
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 20:07:26 +0000

    Mon, Jun 10, 2024, 10:07 PM +02:00

---
+++ game/game.py
@@ -0,0 +1,8 @@
+import random
+
+
+def main():
+    # get user promt n until what number they want to guess
+    print("Enter number of Level from 1 to 100 ")
+    n = int(input("Level: "))
+    guess = random.randrange

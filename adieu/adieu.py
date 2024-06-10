02f5ee33b4594b781dbd9cfbfb0a9aaf6c9096ea
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 19:33:57 +0000

    Mon, Jun 10, 2024, 9:33 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,7 +1,30 @@
 import inflect
+import sys
 
 
 
 def main():
+    p = inflect.engine()
 
-    #
+    #create initial name var
+    names = []
+    print("Enter names, one per line. ")
+
+    #to handle errors we use try and exeptions
+    try:
+        while True:
+             #get Input prompt
+            name = input()
+            names.append(name)
+    except EOFError:
+        pass
+
+    #Check at lear one name has entered
+    if not names:
+        sys.exit("No manes has entered")
+
+    formatted_name = p.join(names)
+    print(f"Adieu, adieu, to {formatted_name}")
+
+main()
+

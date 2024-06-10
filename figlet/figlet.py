9b89e58acbd9bf8fb9a49786a7c0904acaf96d0e
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-10 18:39:00 +0000

    Mon, Jun 10, 2024, 8:39 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,12 @@
-from pyfiglet import Figlet
+from pyfiglet import Figlet,FigletFont
 import sys
+import random
 
-figlet = Figlet()
+
+def main():
+    ag
+
+
 if len(sys.argv) < 2:
     sys.exit("Enter at leat two argument")
 elif len(sys.argv) > 2:

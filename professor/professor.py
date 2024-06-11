bced797780f9ccf88070a3e8892c5d90a1a77c63
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-11 07:49:39 +0000

    Tue, Jun 11, 2024, 9:49 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -6,7 +6,15 @@ def main():
 
 
 def get_level():
-    
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in ['1', '2', '3']:
+                return level
+            else:
+                print("Invalid level. Please enter 1, 2, or 3.")
+        except ValueError:
+            print("Enter valid integer")
 
 
 def generate_integer(level):

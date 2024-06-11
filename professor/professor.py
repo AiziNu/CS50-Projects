a3f68289c07dee75a1c6df80687f71946b9bd4d3
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-11 08:05:31 +0000

    Tue, Jun 11, 2024, 10:05 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,7 +2,37 @@ import random
 
 
 def main():
-    ...
+    # we initializing our var score and call our level function
+    score = 0
+    level = get_level()
+
+    #loop and create var X and Y
+    for i in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        correct_answer = x + y
+        prompt_try = 0
+
+
+        #loop while propt is less than 3
+        while prompt_try < 3:
+            try:
+                user_answer = int(input(f'{x} + {y} = '))
+                if user_answer == correct_answer:
+                    score +=1
+                    break
+                else:
+                    print("EEE")
+            except ValueError:
+                print("EEE")
+
+        if prompt_try == 3:
+            print(f"{x} + {y} = {correct_answer}")
+
+    print(f'Score: {score}')
+
+
+
 
 
 def get_level():
@@ -18,4 +48,14 @@ def get_level():
 
 
 def generate_integer(level):
-    ...
+    if level == '1':
+        return random.randint(0,9)
+    elif level == '2':
+        return random.randint(10,99)
+    elif level == '3':
+        return random.randint(100,999)
+    else:
+        raise ValueError("Enter correct level. It must be 1,2 or 3")
+
+
+main()

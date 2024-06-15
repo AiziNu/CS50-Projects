dc0948927049e9d78dc093ecac2bb02fa803b70f
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-15 14:12:40 +0000

    Sat, Jun 15, 2024, 4:12 PM +02:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -5,7 +5,11 @@ def main():
     test_twttr()
 
 def test_twttr():
-    try:
-        assert convert_twttr("TOMORROW") == "tmttw"
-        assert convert_twttr("TWitter") == "twttr"
+    assert convert_twttr("TOMORROW") == "tmttw"
+    assert convert_twttr("TWitter") == "twttr"
+    assert convert_twttr("hello") == "hll"
+    assert convert_twttr("12345") == "12345"
+    assert convert_twttr("Symbol$%^") == "smbl$%^"
+    assert
+
 

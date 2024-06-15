09c7a8f89416242748e8511de1ca7b6620c81f25
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-15 19:55:08 +0000

    Sat, Jun 15, 2024, 9:55 PM +02:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -1,33 +1,20 @@
-from twttr import convert_twttr
+from twttr import shorten
 
-def test_no_vowels():
-    assert convert_twttr("bcdfghjklmnpqrstvwxyz") == "bcdfghjklmnpqrstvwxyz"
+def test_shorten_no_vowels():
+    assert shorten("rhythm") == "rhythm"
 
-def test_only_vowels():
-    assert convert_twttr("AEIOUaeiou") == ""
+def test_shorten_mixed_case():
+    assert shorten("Twitter") == "Twttr"
 
-def test_mixed_case():
-    assert convert_twttr("hello") == "hll"
-    assert convert_twttr("HELLO") == "HLL"
-    assert convert_twttr("hElLo") == "hlL"
+def test_shorten_all_vowels():
+    assert shorten("AEIOUaeiou") == ""
 
-def test_numbers_and_special_chars():
-    assert convert_twttr("12345") == "12345"
-    assert convert_twttr("!@#$%") == "!@#$%"
-    assert convert_twttr("Python 3.8") == "Pythn 3.8"
+def test_shorten_empty_string():
+    assert shorten("") == ""
 
-def test_edge_cases():
-    assert convert_twttr("") == ""
-    assert convert_twttr("A") == ""
-    assert convert_twttr("b") == "b"
-    assert convert_twttr(" ") == " "
-    assert convert_twttr("a e i o u") == "    "
-    assert convert_twttr("!@#$%^&*()") == "!@#$%^&*()"
+def test_shorten_sentence():
+    sentence = "Hello, World!"
+    expected = "Hll, Wrld!"
+    assert shorten(sentence) == expected
 
-def test_mixed_content():
-    assert convert_twttr("Python Programming is Fun!") == "Pythn Prgrmmng s Fn!"
-    assert convert_twttr("Test 123!@#") == "Tst 123!@#"
 
-if __name__ == "__main__":
-    import pytest
-    pytest.main()

e02008dcae449223d88660b882996c58712bd59e
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-15 20:22:45 +0000

    Sat, Jun 15, 2024, 10:22 PM +02:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -1,4 +1,20 @@
 from bank import value
 
+def test_value_hello():
+    assert value("hello") == 0
 
+def test_value_casesensetive():
+    assert value("HeLlo") == 0
+
+def test_value_h():
+    assert value('hi') == 20
+
+def test_value_h_casesensetive():
+    assert value('Hey') == 20
+
+def test_value_other():
+    assert value("goodbye") == 100
+
+def test_value_emty_value():
+    assert value("") == 100
 

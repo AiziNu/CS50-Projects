910e9cee6cd601439ce6cb1f5673e2bc0df6c16c
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-15 20:10:41 +0000

    Sat, Jun 15, 2024, 10:10 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -8,3 +8,22 @@ elif greeting.startswith("h"):
     print("$20")
 else:
     print("$100")
+
+
+
+def main():
+    ...
+
+
+def value(greeting):
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+
+if __name__ == "__main__":
+    main()

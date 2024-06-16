81aed797268e26b5ffedf2445b3fdd042fe55551
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-16 13:34:38 +0000

    Sun, Jun 16, 2024, 3:34 PM +02:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -5,7 +5,7 @@ def test_convert_valid():
     assert convert("1/2") == 50
     assert convert("3/4") == 75
     assert convert("0/1") == 0
-    assert convert("1/1") == 100
+
 
 def test_convert_invalid():
     with pytest.raises(ValueError):
@@ -14,16 +14,11 @@ def test_convert_invalid():
         convert("2/1")
     with pytest.raises(ValueError):
         convert("1/0")
-    with pytest.raises(ValueError):
-        convert("3/2")
 
 def test_gauge():
-    assert gauge(0) == "E"
     assert gauge(1) == "E"
     assert gauge(99) == "F"
-    assert gauge(100) == "F"
     assert gauge(50) == "50%"
-    assert gauge(75) == "75%"
 
 if __name__ == "__main__":
     pytest.main()

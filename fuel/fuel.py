3ccb951fc5d4dc2ca82394489645afc8c8e837aa
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-16 12:18:40 +0000

    Sun, Jun 16, 2024, 2:18 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,4 +1,10 @@
+
+
 def main():
+    ...
+
+
+def convert(fraction):
     while True:
         try:
             #get prompt from user
@@ -15,26 +21,10 @@ def main():
 
             percentage = (x/y)*100
 
-            if percentage <= 1:
-                print("E")
-            elif percentage >=99:
-                print("F")
-            else:
-                print(f"{round(percentage)}%")
-            break
-
 
         except (ValueError, ZeroDivisionError):
             # Prompt the user again if input is invalid
             pass
-
-main()
-
-def main():
-    ...
-
-
-def convert(fraction):
 
 
 

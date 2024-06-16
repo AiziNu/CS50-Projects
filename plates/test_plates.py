d6485c399a4bee7ddabb3d6ca38199da34b1b67c
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-16 10:39:10 +0000

    Sun, Jun 16, 2024, 12:39 PM +02:00

--- plates/test_plates.py
+++ plates/test_plates.py
@@ -18,3 +18,16 @@ def test_is_valid_starts_alphabetical():
 
 def test_is_valid_all_letters():
     assert is_valid("ABCDEF") == True
+    assert is_valid("A") == False
+    assert is_valid("AB") == True
+    assert is_valid("A1B") == False
+
+def test_is_valid_numbers_not_in_middle():
+    assert is_valid("AB123") == True
+    assert is_valid("A1B23") == False
+    assert is_valid("AB12C") == False
+
+def test_is_valid_no_leading_zero():
+    assert is_valid("AB123") == True
+    assert is_valid("AB012") == False
+    assert is_valid("A0123B") == False

7c96cab464f755553e260e8bd5b453051ae825cb
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-16 10:21:06 +0000

    Sun, Jun 16, 2024, 12:21 PM +02:00

--- plates/test_plates.py
+++ plates/test_plates.py
@@ -2,4 +2,16 @@ from plates import is_valid
 
 
 def test_is_valid_length():
-    assert is_valid("")
+    assert is_valid("AB123") == True
+    assert is_valid("AB12325") == False
+    assert is_valid("a") == False
+
+def test_is_valid_alphanumeric():
+    assert is_valid("AB123") == True
+    assert is_valid("AB1 325") == False
+    assert is_valid("AB34!") == False
+
+def test_is_valid_starts_alphabetical():
+    assert is_valid("AB556") == True
+    assert is_valid("1ab325") == False
+    assert is_valid("!Abi25") == False

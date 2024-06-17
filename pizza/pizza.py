2bbd0c685358e0a05136a4582b7385bf3f519c6a
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-17 20:15:59 +0000

    Mon, Jun 17, 2024, 10:15 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -4,14 +4,14 @@ from tabulate import tabulate
 
 # Check if exactly one command-line argument is given
 if len(sys.argv) != 2:
-    print("Usage: python pizza.py <filename>")
+    print("Too few command-line arguments")
     sys.exit()
 
 filename = sys.argv[1]
 
 # Check if the file name ends with .csv
 if not filename.endswith('.csv'):
-    print("File must be a CSV file with a .csv extension")
+    print("Not a CSV file")
     sys.exit()
 
 try:
@@ -26,3 +26,21 @@ try:
 except FileNotFoundError:
     print(f"File {filename} does not exist")
     sys.exit()
+
+
+def main():
+    #call if aour iptut pass all parameter
+    check_command_line_args()
+
+    try:
+        
+
+def check_command_line_args():
+    # Check the number of command line arguments
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    # Check if the file has a .py extension
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")

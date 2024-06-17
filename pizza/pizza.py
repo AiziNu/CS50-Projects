5b50ef94b1a01430b96a39ee5c6f136404ed9ace
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-17 20:21:16 +0000

    Mon, Jun 17, 2024, 10:21 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,38 +2,25 @@ import sys
 import csv
 from tabulate import tabulate
 
-# Check if exactly one command-line argument is given
-if len(sys.argv) != 2:
-    print("Too few command-line arguments")
-    sys.exit()
-
-filename = sys.argv[1]
-
-# Check if the file name ends with .csv
-if not filename.endswith('.csv'):
-    print("Not a CSV file")
-    sys.exit()
-
-try:
-    # Read the CSV file into a list of lists
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)
-
-    # Use tabulate to print the table in grid format
-    print(tabulate(data, headers="firstrow", tablefmt="grid"))
-
-except FileNotFoundError:
-    print(f"File {filename} does not exist")
-    sys.exit()
-
 
 def main():
     #call if aour iptut pass all parameter
     check_command_line_args()
 
     try:
-        
+    # Read the CSV file into a list of lists
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            data = list(reader)
+
+    # Use tabulate to print the table in grid format
+        print(tabulate(data, headers="firstrow", tablefmt="grid"))
+
+    except FileNotFoundError:
+        print(f"File {filename} does not exist")
+        sys.exit()
+
+
 
 def check_command_line_args():
     # Check the number of command line arguments
@@ -44,3 +31,5 @@ def check_command_line_args():
     # Check if the file has a .py extension
     if not sys.argv[1].endswith(".py"):
         sys.exit("Not a Python file")
+if __name__ == "__main__":
+    main()

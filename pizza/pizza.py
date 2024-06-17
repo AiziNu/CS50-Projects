d3e3a3e13a31f6982f6cd7c02d01f9c189b95146
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-17 20:24:48 +0000

    Mon, Jun 17, 2024, 10:24 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -28,8 +28,13 @@ def check_command_line_args():
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
-    # Check if the file has a .py extension
-    if not sys.argv[1].endswith(".csv"):
-        sys.exit("File does not exist")
+
+  # Check if the file has a .csv extension
+    filename = sys.argv[1]
+    if not filename.endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    return filename
+
 if __name__ == "__main__":
     main()

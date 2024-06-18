689964ac1460404ad891f2717f3881d8ffaa72d4
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-18 17:25:29 +0000

    Tue, Jun 18, 2024, 7:25 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -10,7 +10,7 @@ def check_command_line_args():
     # Check the number of command line arguments
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
+    elif len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
 
   # Check if the file has a .csv extension

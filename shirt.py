cd583a9095fbad5b85f6904909c2ebfb650a12e2
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-18 18:07:47 +0000

    Tue, Jun 18, 2024, 8:07 PM +02:00

--- shirt.py
+++ shirt.py
@@ -2,11 +2,27 @@ import sys
 
 
 def main():
+    #call our check arg funct that out agt needs to pass all conditions
 
 
 
 
 
 def check_command_line_args():
-    if len(sys.argv) < 2
+    # Check the number of command line arguments
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+  # Check if the input file has a .csv extension
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    if not input_filename.endswith(".csv"):
+        sys.exit("Input file is not a CSV file")
+    if not output_filename.endswith(".csv"):
+        sys.exit("Output file is not a CSV file")
+
+    return input_filename, output_filename
+
 

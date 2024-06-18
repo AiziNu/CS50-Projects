9ec4268e1f0967bd8a2dfb1e1f3917b451a416f5
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-18 17:39:27 +0000

    Tue, Jun 18, 2024, 7:39 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,9 +1,10 @@
 def main():
     #call check agument funct that it has to pass all condition
+    input_file, output_file = check_command_line_args()
 
     #we use try catch if there anyy eroor will occur
     try:
-        with open("before.csv") as 
+        with open(input_file, mode = 'r') as
 
 
 
@@ -20,9 +21,12 @@ def check_command_line_args():
     elif len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
 
-  # Check if the file has a .csv extension
-    filename = sys.argv[1]
-    if not filename.endswith(".csv"):
-        sys.exit("Not a CSV file")
+  # Check if the input file has a .csv extension
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    if not input_filename.endswith(".csv"):
+        sys.exit("Input file is not a CSV file")
+    if not output_filename.endswith(".csv"):
+        sys.exit("Output file is not a CSV file")
 
-    return filename
+    return input_filename, output_filename

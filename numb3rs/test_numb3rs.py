a82caad4d1d6c894fba91eff5bf81437af5fb0ff
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-25 07:01:41 +0000

    Tue, Jun 25, 2024, 9:01 AM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,16 +4,16 @@ def test_valid_addresses():
     assert validate("127.0.0.1") == True
     assert validate("0.0.0.0") == True
     assert validate("1.2.3.4") == True
+    assert validate("255.255.255.0") == True
+
 
 
 def test_invalid_addresses():
-    assert validate("75.456.76.65") == False
+    assert validate("275.3.6.28") == False
     assert validate("192.168.0") == False
     assert validate("192.168.0.abc") == False
-    assert validate("") == False
-    assert validate("123.045.067.089") == False  # Leading zeros invalid
 
 def test_edge_cases():
     assert validate("0.0.0.256") == False
     assert validate("0.0.0.-1") == False
-    assert validate(" 0.0.0.0") == False
+    assert validate("cat") == False

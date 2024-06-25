fb5b6ae6ba960f6226d3f7f63f770a5da4f652fb
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-25 19:42:45 +0000

    Tue, Jun 25, 2024, 9:42 PM +02:00

--- working/test_working.py
+++ working/test_working.py
@@ -5,8 +5,6 @@ def test_valid_times():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("12:00 AM to 12:00 PM") == "00:00 to 12:00"
-    assert convert("1 PM to 1 AM") == "13:00 to 01:00"
-    assert convert("11:59 PM to 12:01 AM") == "23:59 to 00:01"
 
 def test_invalid_times():
     with pytest.raises(ValueError):

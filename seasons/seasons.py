bc10717e28b459d353d5f1e0a221936b6dee2ae0
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-27 17:57:55 +0000

    Thu, Jun 27, 2024, 7:57 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,17 +1,25 @@
-import pytest
-from datetime import date
-from seasons import calculate_age_in_minutes, minutes_to_words
+from datetime import date, datetime
+import inflect
+import sys
 
-def test_calculate_age_in_minutes():
-    assert calculate_age_in_minutes(date(2000, 1, 1), date(2001, 1, 1)) == 366 * 24 * 60  # Includes a leap year
-    assert calculate_age_in_minutes(date(2020, 1, 1), date(2021, 1, 1)) == 366 * 24 * 60  # Leap year check
-    assert calculate_age_in_minutes(date(2021, 1, 1), date(2022, 1, 1)) == 365 * 24 * 60  # Non-leap year check
+def main():
+    try:
+        dob = input("Date of Birth (YYYY-MM-DD): ")
+        birth_date = datetime.strptime(dob, "%Y-%m-%d").date()
+    except ValueError:
+        sys.exit("Invalid date format")
 
-def test_minutes_to_words():
-    assert minutes_to_words(525600) == "five hundred twenty five thousand six hundred"
-    assert minutes_to_words(527040) == "five hundred twenty seven thousand forty"  # for a leap year
-    assert minutes_to_words(527041) == "five hundred twenty seven thousand forty one"
+    today = date.today()
+    age_in_minutes = calculate_age_in_minutes(birth_date, today)
+    print(minutes_to_words(age_in_minutes))
 
-# Running the tests
+def calculate_age_in_minutes(birth_date, today):
+    delta = today - birth_date
+    return delta.days * 24 * 60
+
+def minutes_to_words(minutes):
+    p = inflect.engine()
+    return p.number_to_words(minutes, andword="").replace(",", "").replace("-", " ")
+
 if __name__ == "__main__":
-    pytest.main()
+    main()
--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,6 +1,17 @@
 import pytest
+from datetime import date
+from seasons import calculate_age_in_minutes, minutes_to_words
+
+def test_calculate_age_in_minutes():
+    assert calculate_age_in_minutes(date(2000, 1, 1), date(2001, 1, 1)) == 366 * 24 * 60  # Includes a leap year
+    assert calculate_age_in_minutes(date(2020, 1, 1), date(2021, 1, 1)) == 366 * 24 * 60  # Leap year check
+    assert calculate_age_in_minutes(date(2021, 1, 1), date(2022, 1, 1)) == 365 * 24 * 60  # Non-leap year check
 
 def test_minutes_to_words():
     assert minutes_to_words(525600) == "five hundred twenty five thousand six hundred"
     assert minutes_to_words(527040) == "five hundred twenty seven thousand forty"  # for a leap year
     assert minutes_to_words(527041) == "five hundred twenty seven thousand forty one"
+
+# Running the tests
+if __name__ == "__main__":
+    pytest.main()

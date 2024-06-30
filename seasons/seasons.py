3b6574642fe9a23c3fbca870375e9bc7315141df
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-30 14:01:16 +0000

    Sun, Jun 30, 2024, 4:01 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,12 +8,13 @@ def main():
     except ValueError():
         sys.exit("Invalid date")
 
-    print(minutes_lived(year, month, day))
+    minutes_lived(year, month, day)
 
 
 
 def minutes_lived(year,month, day):
     dt = date(int(year), int(month), int(day))
+    print(dt)
 
 
 if __name__ == "__main__":

d008522c0f45446200b8b06c468a90ac8bb33216
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-30 13:16:07 +0000

    Sun, Jun 30, 2024, 3:16 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,10 +2,11 @@ from datetime import date
 
 
 def main():
+    year, month, day = input("Date of birth: ").split("-")
     ...
 
 
-def minutes_lived(year,mo)
+def minutes_lived(year,month, day)
 
 
 if __name__ == "__main__":

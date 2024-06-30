7ff1764180b16c840a5b16a04ec9df76e7aa31f8
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-30 09:09:44 +0000

    Sun, Jun 30, 2024, 11:09 AM +02:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -27,7 +27,37 @@ def test_str():
 
 
 def test_deposit():
-    ...
+    jar = Jar()
+    jar.deposit(5)
+    assert jar.size == 5
+
+    with pytest.raises(ValueError):
+        jar.deposit(10)
 
 
 def test_withdraw():
+    jar = Jar()
+    jar.deposit(5)
+    jar.withdraw(3)
+    assert jar.size == 2
+
+    with pytest.raises(ValueError):
+        jar.withdraw(3)
+
+def test_capacity():
+    jar = Jar(15)
+    assert jar.capacity == 15
+    jar = Jar()
+    assert jar.capacity == 12
+
+def test_size():
+    jar = Jar()
+    assert jar.size == 0
+    jar.deposit(7)
+    assert jar.size == 7
+    jar.withdraw(4)
+    assert jar.size == 3
+
+if __name__ == "__main__":
+    pytest.main()
+

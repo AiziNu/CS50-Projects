e48fb0db93856c189710dbd0dd50ee93dc2537ec
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-06-30 08:46:01 +0000

    Sun, Jun 30, 2024, 10:46 AM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -3,13 +3,16 @@
 class Jar:
     def __init__(self, capacity=12):
         if not isinstance(n,int) or capacity < 0:
-            raise ValueError()
+            raise ValueError("Capacity must be non-negative int")
+        self._capacity = capacity
+        self._cookies = 0
 
     def __str__(self):
-        ...
+        return "🍪" * self._cookies
 
     def deposit(self, n):
-        ...
+        if 
+
 
     def withdraw(self, n):
         ...

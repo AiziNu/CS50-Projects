cbc74cf46d9e8a2cb68a88b0f83933419e726caf
Aizirek Nuralieva <145041411+AiziNu@users.noreply.github.com>
2024-07-04 17:28:42 +0000

    Thu, Jul 4, 2024, 7:28 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,41 +1,32 @@
 import random
 
-
-
 def main():
-    # we initializing our var score and call our level function
-    score = 0
     level = get_level()
+    score = 0
 
-    #loop and create var X and Y
     for _ in range(10):
         x = generate_integer(level)
         y = generate_integer(level)
         correct_answer = x + y
-        prompt_try = 0
 
-
-        #loop while propt is less than 3
-        while prompt_try < 3:
+        attempts = 0
+        while attempts < 3:
             try:
-                user_answer = int(input(f'{x} + {y} = '))
+                user_answer = int(input(f"{x} + {y} = "))
                 if user_answer == correct_answer:
-                    score +=1
+                    score += 1
                     break
                 else:
                     print("EEE")
-                    prompt_try += 1
+                    attempts += 1
             except ValueError:
                 print("EEE")
-                prompt_try += 1
+                attempts += 1
 
-        if prompt_try == 3:
+        if attempts == 3:
             print(f"{x} + {y} = {correct_answer}")
-    print(f'Score: {score}/10')
 
-
-
-
+    print(f"Score: {score}/10")
 
 def get_level():
     while True:
@@ -43,21 +34,18 @@ def get_level():
             level = int(input("Level: "))
             if level in [1, 2, 3]:
                 return level
-            else:
-                print("Invalid level. Please enter 1, 2, or 3.")
         except ValueError:
-            print("Enter valid integer")
-
+            pass  # Ignore invalid input and prompt again
 
 def generate_integer(level):
     if level == 1:
-        return random.randint(0,9)
+        return random.randint(0, 9)
     elif level == 2:
-        return random.randint(10,99)
+        return random.randint(10, 99)
     elif level == 3:
-        return random.randint(100,999)
+        return random.randint(100, 999)
     else:
-        raise ValueError("Enter correct level. It must be 1, 2 or 3")
+        raise ValueError("Invalid level")
 
-
-main()
+if __name__ == "__main__":
+    main()
